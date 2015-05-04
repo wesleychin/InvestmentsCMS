dd5ca439d3071593c30693a8a32d4862a7f72b1f
Wesley Chin <wesley@databuild.co.za>
2015-05-04 19:35:11 +0200

    Added the initial files with yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name investmentsCmsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the investmentsCmsApp
+ */
+angular.module('investmentsCmsApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
